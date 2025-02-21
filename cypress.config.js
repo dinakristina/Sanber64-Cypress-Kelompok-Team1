e73d504809412821ca6b5a19c14a296eeae9533b
unknown <afryank206@gmail.com>
2025-02-21 19:02:21 +0700

    buat new branch baru

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  pageLoadTimeout: 200_000,
+  viewportWidth: 1280,
+  viewportHeight: 720,
+
+  e2e: {
+    experimentalStudio:true,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
